b095c63dc24b855b73bc5a7c7d9d3d5332cad840
kazuma takiuchi <basstrombone413@gmail.com>
2018-08-22 05:28:44 +0900

    fix server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
 
 app.get("/", function(req, res) {
+  getTrend();
   res.send("Hello World");
 });
 
 console.log("start", "http://localhost:3000");
 
 app.listen(3000);
+
+const getTrend = async function() {
+  let res = await fetch("https://qiita.com/api/v2/items");
+  let json = await res.json();
+  console.log("json:", json);
+
+  return json;
+};
