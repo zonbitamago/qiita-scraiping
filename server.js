5507ee6ccf51e9e4f9c14503aef45c8e35c4109f
kazuma takiuchi <basstrombone413@gmail.com>
2018-08-24 05:03:06 +0900

    fix apiURL

--- server.js
+++ server.js
@@ -12,9 +12,15 @@ console.log("start", "http://localhost:3000");
 app.listen(3000);
 
 const getTrend = async function() {
-  let res = await fetch("https://qiita.com/api/v2/items");
+  let res = await fetch(
+    "https://qiita.com/api/v2/items?query=created%3A>2018-08-21+created%3A<2018-08-22&per_page=100"
+  );
   let json = await res.json();
-  console.log("json:", json);
+
+  json.map(item => {
+    console.log("item.created_at:", item.created_at);
+  });
+  //   console.log("json:", json);
 
   return json;
 };
