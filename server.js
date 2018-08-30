24f40b06f3a4fce49b601b62c4c060c2566724f8
kazuma takiuchi <basstrombone413@gmail.com>
2018-08-31 05:31:00 +0900

    fix paging

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
 const moment = require("moment");
-const puppeteer = require("puppeteer");
 
 app.get("/", function(req, res) {
   getTrend();
@@ -14,15 +13,12 @@ console.log("start", "http://localhost:3000");
 app.listen(3000);
 
 const getTrend = async function() {
-  const count = await getSerchPageCount();
   const peerPage = 100;
-  let maxPage = Math.floor(count / peerPage);
-  if (count % peerPage > 0) {
-    maxPage++;
-  }
-
+  let page = 1;
+  let hasNextPage = true;
   const conditionDate = getSerchDate();
-  for (var page = 1; page <= maxPage; page++) {
+
+  while (hasNextPage) {
     const url =
       "https://qiita.com/api/v2/items?query=" +
       "created%3A>" +
@@ -41,31 +37,17 @@ const getTrend = async function() {
     let json = await res.json();
 
     console.log("json.length:", json.length);
+
+    if (res.status != 200 || json.length < peerPage) {
+      hasNextPage = false;
+    } else {
+      page++;
+    }
   }
 
+  console.log("finish fetch");
+
   return "finish";
-};
-
-const getSerchPageCount = async function() {
-  const conditionDate = getSerchDate();
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const url =
-    "https://qiita.com/search?utf8=%E2%9C%93&sort=created&q=created%3A%3E" +
-    conditionDate.fromDate +
-    "+created%3A%3C" +
-    conditionDate.toDate;
-  await page.goto(url);
-  const count = await page.evaluate(function() {
-    return document.querySelector(
-      "#main > div > div > div.searchResultContainer_main > div.searchResultContainer_navigation > ul > li.active > a > span"
-    ).innerText;
-  });
-
-  console.log("count:", count);
-
-  await browser.close();
-  return count;
 };
 
 const getSerchDate = function() {
